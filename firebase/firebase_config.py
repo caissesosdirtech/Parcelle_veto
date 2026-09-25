e28b13960b601d4b0c44b86e94a8c67f0ba4175b
madamel <caissesociale2025@gmailcom>
2026-09-25 18:22:18 +0000

    Ajout systeme notification sonnore

---
+++ firebase/firebase_config.py
@@ -0,0 +1,17 @@
+import os
+
+import firebase_admin
+from firebase_admin import credentials
+from django.conf import settings
+
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(
+        os.path.join(
+            settings.BASE_DIR,
+            "firebase",
+            "serviceAccountKey.json",
+        )
+    )
+
+    firebase_admin.initialize_app(cred)
