ba44417769b93d35c8fbd53653411fdd0cf52728
madamel <caissesociale2025@gmailcom>
2026-09-26 16:19:39 +0000

    Ajout du champ fcm_token

--- accounts/models.py
+++ accounts/models.py
@@ -2,22 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Utilisateur(AbstractUser):
-    # Vos choix de rôles existants...
     ROLE_CHOICES = [
         ('DOCTEUR', 'Docteur Vétérinaire'),
         ('ASSISTANT', 'Employé / Assistant'),
         ('PHARMACIEN', 'Pharmacien'),
     ]
-    
+
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, blank=True, null=True)
+    fcm_token = models.CharField(max_length=255, blank=True, null=True)  # 👈 ce champ
 
     @property
     def libelle_role(self):
-        """
-        Retourne 'Superadmin' si l'utilisateur est un superuser Django,
-        sinon retourne son rôle attribué (Docteur, Assistant, etc.).
-        Fonctionne à l'identique en Local et en Production.
-        """
         if self.is_superuser:
             return "Superadmin"
         return self.get_role_display() if self.role else "Utilisateur"
